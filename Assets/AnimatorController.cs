77f739e56408b4441df370a57e6790de4f0820e4
Federico Simonetti <federicosimonetti94@gmail.com>
2017-07-28 15:21:43 +0200

    dd

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour {
+
+    public Animator anim;
+    private int currentState=0;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {            
+            anim.Play("pezzo5", -1, 0f);
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.Play("pezzo4", -1, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.Play("pezzo3", -1, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            anim.Play("pezzo2", -1, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            anim.Play("pezzo1", -1, 0f);
+        }
+        
+
+
+    }
+}
